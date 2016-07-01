7f6f3acb6babe71fa65d7d05fbb1167c4751fb76
Sashtastic <ht.sasha.tran@gmail.om>
2016-06-30 21:01:31 -0400

    What is a function?....

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-var $section1 = $('.section1');
-var $section2 = $('.section2');
-var $section3 = $('.section3');
+var $icon = $('.icon');
 
-$section1.waypoint(function () {
-  $section1.addClass('svg-pop-up');
-}, {offset: '50%'});
-
-$section2.waypoint(function () {
-  $section2.addClass('svg-pop-up');
-}, {offset: '50%'});
-
-$section3.waypoint(function () {
-  $section3.addClass('svg-pop-up');
+$icon.waypoint(function () {
+  $icon.addClass('js-svg-animate');
 }, {offset: '50%'});
